3d9ae7a6b4ced6599bdf12644ce8b71848fb24f6
GitHub git update Action <action@github.com>
2024-06-09 06:52:51 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_VTHPNP.h
+++ generated/controllers/generated/signature_VTHPNP.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2583412487
-#define TS_SIGNATURE "rusEFI main.2024.06.09.VTHPNP.2583412487"
+#define SIGNATURE_HASH 1388547600
+#define TS_SIGNATURE "rusEFI main.2024.06.09.VTHPNP.1388547600"
